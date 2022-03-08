47d383b93a9b8d14b34699914b12f182aa1f67d2
raomanoj <manoj9431yadav@gmail.com>
2022-03-08 22:34:41 +0530

    js data type done

---
+++ html/learning/index.js
@@ -0,0 +1,50 @@
+// starting js -> JAVA SCRIPT
+// var let const
+ 
+// variable declaration
+var a; // int string float ?
+
+// variable initalization
+
+a = 10;
+console.log(typeof a);
+console.log(a);
+
+a = "hello";  // re initialize of variable 'a'
+console.log(typeof a);
+console.log(a);
+
+//type of operators -> it tells us the types of data stored in a variable
+console.log(typeof a);
+
+a = true;
+console.log(a);
+console.log(typeof a);
+
+a = null;
+console.log(a);
+console.log(typeof a);
+
+//Number
+var num =10;
+console.log(num);
+var float = 2.4;
+console.log(float);
+
+//Bollean
+var t = (true);
+var f = (false);
+console.log(t);
+console.log(f);
+
+//string
+//'' , "" , ` `
+// single quotes , double quotes , backtick
+var str = (a);
+console.log(typeof str);
+console.log(str);
+
+
+
+
+
